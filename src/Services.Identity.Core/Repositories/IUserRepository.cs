076c11b0f41e1875abd56346deb727434f835366
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-15 18:45:04 +0200

    Created IUserRepository

---
+++ src/Services.Identity.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Tasks;
+using Services.Identity.Core.Entities;
+
+namespace Services.Identity.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetAsync(Guid id);
+        Task<User> GetAsync(string email);
+        Task AddAsync(User user);
+    }
+}
