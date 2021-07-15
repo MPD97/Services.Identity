8c0c48fbee2503eb823f4ab05843dc621c08dc41
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-15 18:06:51 +0200

    Created User entity

---
+++ src/Services.Identity.Core/Entities/User.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using Services.Identity.Core.Exceptions;
+
+namespace Services.Identity.Core.Entities
+{
+    public class User : AggregateRoot
+    {
+        public string Email { get; }
+        public string Role { get; }
+        public string Password { get; }
+        public DateTime CreatedAt { get; }
+        public IEnumerable<string> Permissions { get; }
+
+        public User(Guid id, string email, string role, string password, DateTime createdAt, 
+            IEnumerable<string> permissions = null)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new InvalidEmailException();
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new InvalidPasswordException();
+            }
+            
+            Id = id;
+            Email = email;
+            Role = role;
+            Password = password;
+            CreatedAt = createdAt;
+            Permissions = permissions;
+        }
+    }
+}
