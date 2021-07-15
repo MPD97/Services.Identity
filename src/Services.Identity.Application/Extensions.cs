efb269bfda49fa7bb9f4c01a2ae318d11394e648
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-15 19:14:43 +0200

    Update application extension

--- src/Services.Identity.Application/Extensions.cs
+++ src/Services.Identity.Application/Extensions.cs
@@ -1,18 +1,16 @@
 using Convey;
-using Microsoft.AspNetCore.Builder;
+using Convey.CQRS.Commands;
+using Convey.CQRS.Events;
 
 namespace Services.Identity.Application
 {
     public static class Extension
     {
-        public static IConveyBuilder AddApplication(this IConveyBuilder builder)
-        {
-            return builder;
-        }
-    
-        public static IApplicationBuilder UseApplication(this IApplicationBuilder builder)
-        {
-            return builder;
-        }
+        public static IConveyBuilder AddApplication(this IConveyBuilder builder) 
+        => builder
+            .AddCommandHandlers()
+            .AddEventHandlers()
+            .AddInMemoryCommandDispatcher()
+            .AddInMemoryEventDispatcher();
     }
 }
