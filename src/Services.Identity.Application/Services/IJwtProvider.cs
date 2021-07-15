c5f33f6703d8dfd3c6e2bae73698c59ac0d88ceb
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-15 20:00:20 +0200

    Created IJwtProvider

---
+++ src/Services.Identity.Application/Services/IJwtProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Services.Identity.Application.DTO;
+
+namespace Services.Identity.Application.Services
+{
+    public interface IJwtProvider
+    {
+        AuthDto Create(Guid userId, string role, string audience = null,
+            IDictionary<string, IEnumerable<string>> claims = null);
+    }
+}
