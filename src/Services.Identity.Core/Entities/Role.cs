02d29aa3e69940f03a32905b250a5a3f59395ba8
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-15 18:14:20 +0200

    Created Role

---
+++ src/Services.Identity.Core/Entities/Role.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Services.Identity.Core.Entities
+{
+    public class Role
+    {
+        public const string User = "user";
+        public const string Admin = "admin";
+        
+        private const StringComparison StringComparison = System.StringComparison.Ordinal;
+        
+        public static bool IsValid(string role)
+        {
+            if (string.IsNullOrWhiteSpace(role))
+            {
+                return false;
+            }
+
+            return string.Equals(role, User, StringComparison) 
+                   || string.Equals(role, Admin, StringComparison);
+        }
+    }
+}
