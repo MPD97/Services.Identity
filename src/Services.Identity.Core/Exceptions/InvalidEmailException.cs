555e26be82135ffe541e7ac4d0dea63fb977557d
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-15 21:07:27 +0200

    Created exceptions

--- src/Services.Identity.Core/Exceptions/InvalidEmailException.cs
+++ src/Services.Identity.Core/Exceptions/InvalidEmailException.cs
@@ -3,7 +3,7 @@ namespace Services.Identity.Core.Exceptions
     public class InvalidEmailException : DomainException
     {
         public override string Code => "invalid_email";
-        public InvalidEmailException() : base("Email is not valid")
+        public InvalidEmailException(string email) : base($"Email: {email} is not valid")
         {
         }
     }
---
+++ src/Services.Identity.Core/Exceptions/InvalidRefreshTokenException.cs
@@ -0,0 +1,11 @@
+namespace Services.Identity.Core.Exceptions
+{
+    public class InvalidRefreshTokenException : DomainException
+    {
+        public override string Code => "invalid_refresh_token";
+        
+        public InvalidRefreshTokenException() : base("Invalid refresh token.")
+        {
+        }
+    }
+}
